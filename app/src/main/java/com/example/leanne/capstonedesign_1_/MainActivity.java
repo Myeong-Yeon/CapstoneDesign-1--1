bc45043f0ee09e41ffb6a159cda2415c6566894f
Leanne Lim <limlean590@naver.com>
2016-04-04 14:17:44 +0900

    Removed title bar

--- app/src/main/java/com/example/leanne/capstonedesign_1_/MainActivity.java
+++ app/src/main/java/com/example/leanne/capstonedesign_1_/MainActivity.java
@@ -3,15 +3,19 @@ package com.example.leanne.capstonedesign_1_;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
     private BackPressCloseHandler backPressCloseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         startActivity(new Intent(this, SplashActivity.class));
